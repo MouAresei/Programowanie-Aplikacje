7adeb8af712e5175577f37a2a46e12742c0ec7b2
adam <ewa.juda@e-zsel.kielce.eu>
2022-10-27 13:06:56 +0200

    poprawka

---
+++ Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PolymorphismApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog firstDog = new Dog();
+            Dog secondDog = new Dog("Burek", 5);
+
+            Dog thirdDog = new Dog(secondDog);
+
+            firstDog.ShowInfo();
+            secondDog.Bark();
+            thirdDog.ChasePostman();
+
+            Cat firstCat = new Cat("Mruczek", 12);
+            firstCat.ShowInfo();
+            firstCat.Meow();
+            firstCat.ClimbOnTree();
+        }
+    }
+}
